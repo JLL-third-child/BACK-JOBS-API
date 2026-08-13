189ad3cadda204df29273f385adcec8e69e5741d
JLL-third-child <jaidilisiluna@gmail.com>
2026-08-13 23:12:06 +0000

    Test 2

--- src/test/java/com/uap/proiv/jobs/service/impl/UserJobAssignedServiceImplTest.java
+++ src/test/java/com/uap/proiv/jobs/service/impl/UserJobAssignedServiceImplTest.java
@@ -107,6 +107,8 @@ public class UserJobAssignedServiceImplTest {
         assertNotNull(result);
         assertEquals(2, result.size());
         assertEquals(10, result.get(0).getUsers().get(0).getId());
+        assertEquals(2, result.size());
+
 
         verify(jobService, times(1)).getAllJobs();
         verify(userService, times(1)).search(1);
